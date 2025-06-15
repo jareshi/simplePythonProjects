493ee55520915558eb3c81b77c1621157b64340d
jareshi <reshi@wayne.edu>
2025-06-15 11:42:45 -0400

    Add Number Guessing Game implementation

---
+++ numberGuesser.py
@@ -0,0 +1,54 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+startAttempts = 0  # Number of attempts to start the game
+
+while True:
+    rangeMax = input("Select a positive number: ")
+    startAttempts += 1
+
+    if rangeMax.isdigit():
+        rangeMax = int(rangeMax)
+        if rangeMax <= 0:
+            print("Please enter a positive integer.")
+            if startAttempts == 4:
+                print("Too many invalid attempts. Exiting the game.")
+                quit()
+            continue
+        break
+    else:
+        print("Invalid input. Please enter a positive integer.")
+        if startAttempts == 4:
+            print("Too many invalid attempts. Exiting the game.")
+            quit()
+        continue
+
+secretNumber = random.randint(1, rangeMax)
+guessNumber = 0
+print(f"Alrighty then! I've selected a secret number between 1 and {rangeMax}. Try to guess it!")
+
+while True:
+    guessNumber += 1
+    userGuess = input("Enter your guess: ")
+
+    if userGuess.isdigit():
+        userGuess = int(userGuess)
+        if userGuess < 1 or userGuess > rangeMax:
+            print(f"Your guess must be between 1 and {rangeMax}. Try again.")
+            continue
+    else:
+        print("Invalid input. Please enter a valid number.")
+        continue
+
+    if userGuess < secretNumber:
+        print("Too low! Try again.")
+    elif userGuess > secretNumber:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the secret number {secretNumber}!")
+        break
+
+print(f"It took you {guessNumber} attempts to guess the number.")
+print("Thanks for playing the Number Guessing Game!")
+
+# JHAP
